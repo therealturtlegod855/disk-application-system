10689558685ba023f5fde6d7dca2a6933470d6c5
therealturtlegod855 <therealturtlegod855@gmail.com>
2025-11-28 18:41:54 -0700

    Add basic bootloader in kernel.asm
    Implement basic bootloader functionality including screen clearing, string printing, and reading drive information.

---
+++ kernel.asm
@@ -0,0 +1,47 @@
+bits 16
+org 0x0000
+
+start:
+    call clear_screen
+    mov si, msg1
+    call print_string
+    mov al, [0x7c05]  ; Read passed drive
+    call print_hex
+    mov si, msg2
+    call print_string
+    jmp $
+
+print_hex:
+    push ax
+    mov ah, al
+    shr al, 4
+    call .nibble
+    mov al, ah
+    and al, 0x0F
+    .nibble:
+    cmp al, 10
+    sbb al, 0x69
+    das
+    call print_char
+    pop ax
+    ret
+
+clear_screen:
+    mov ax, 0x0003
+    int 0x10
+    ret
+
+print_string:
+    lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0e
+    int 0x10
+    jmp print_string
+.done:
+    ret
+
+msg1: db "Drive ", 0
+msg2: db " selected", 13, 10, 0
+
+times 512 - ($ - $$) db 0   
