98fe53e782c55d549f21543958556bd2dfea979b
therealturtlegod855 <therealturtlegod855@gmail.com>
2025-11-28 17:37:15 -0700

    Implement clear_screen function in main.asm
    Added clear_screen function to reset video mode.

--- main.asm
+++ main.asm
@@ -14,10 +14,17 @@ print_string:
     mov ah, 0x0e
     int 0x10
     jmp print_string
+clear_screen:
+    pusha
+    mov ax, 0x0003  ; AH=0 (set video mode), AL=3 (80x25 text mode)
+    int 0x10
+    popa
+    ret   
 .done:
     ret
 
 hello_msg: db "Hello, World!", 0
+call clear_screen
 
 times 510 - ($ - $$) db 0
 dw 0xaa55   
