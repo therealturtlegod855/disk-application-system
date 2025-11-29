9a0047100aed1bf433a2224ebefc3f5d51644a27
therealturtlegod855 <therealturtlegod855@gmail.com>
2025-11-28 17:37:54 -0700

    Refactor clear_screen function in main.asm

--- main.asm
+++ main.asm
@@ -1,11 +1,16 @@
-; main.asm - Working 16-bit Hello World Bootloader
 bits 16
 org 0x7c00
 
 start:
+    call clear_screen
     mov si, hello_msg
     call print_string
     jmp $
+
+clear_screen:
+    mov ax, 0x0003  ; Set text mode 3 (80x25, 16 colors)
+    int 0x10
+    ret
 
 print_string:
     lodsb
@@ -14,17 +19,10 @@ print_string:
     mov ah, 0x0e
     int 0x10
     jmp print_string
-clear_screen:
-    pusha
-    mov ax, 0x0003  ; AH=0 (set video mode), AL=3 (80x25 text mode)
-    int 0x10
-    popa
-    ret   
 .done:
     ret
 
 hello_msg: db "Hello, World!", 0
-call clear_screen
 
 times 510 - ($ - $$) db 0
 dw 0xaa55   
