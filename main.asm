3b64b1b22f9a69a8cc36bd7b34f3b4b954d591b5
therealturtlegod855 <therealturtlegod855@gmail.com>
2025-11-28 17:28:56 -0700

    Refactor main.asm for simplified Hello World output
    Refactored assembly code to streamline the Hello World OS implementation.

--- main.asm
+++ main.asm
@@ -1,51 +1,28 @@
 org 0x7c00
-; main.asm - Fixed 32-bit Hello World OS
 
-; Multiboot header
-MAGIC   equ 0x1BADB002
-FLAGS   equ 0
-CHECKSUM equ -(MAGIC + FLAGS)
-
-section .text
-align 4
-    dd MAGIC
-    dd FLAGS
-    dd CHECKSUM
-
-; Stack and entry point
-section .bss
-align 16
-stack_bottom:
-    resb 16384
-stack_top:
-
-section .text
-global _start
-_start:
-    mov esp, stack_top
-    call kernel_main
-    cli
-.hang:
-    hlt
-    jmp .hang
-
-; Only define kernel_main once
-kernel_main:
-    mov ax, 0x07
+start:
+    mov ax, 0x07c0
+    mov ds, ax
     mov es, ax
-    mov edi, 0
-    mov esi, hello_msg
-.print:
+    mov ss, ax
+    mov sp, 0x7c00
+
+    mov si, hello
+    call print
+
+    jmp $
+
+print:
     lodsb
-    test al, al
+    or al, al
     jz .done
-    stosw
-    jmp .print
+    mov ah, 0x0e
+    int 0x10
+    jmp print
 .done:
     ret
 
-section .data
-hello_msg: db "Hello, World!", 0
+hello: db "Hello, World!", 0
 
-times 8192 - ($-$$) db 0   
-dw 0xaa5 
+times 510 - ($ - $$) db 0
+dw 0xaa55   
