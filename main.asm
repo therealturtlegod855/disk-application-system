280577e7f03161de6bcb559d4d2bdbc1f408d1e6
therealturtlegod855 <therealturtlegod855@gmail.com>
2025-11-28 17:57:52 -0700

    Update main.asm to include drive detection

--- main.asm
+++ main.asm
@@ -5,10 +5,68 @@ start:
     call clear_screen
     mov si, hello_msg
     call print_string
+
+    ; Try to detect drives (0x80 = first HDD, 0x00 = first FDD)
+    mov dl, 0x80
+    call print_drive_check
+
+    mov dl, 0x00
+    call print_drive_check
+
     jmp $
 
+print_drive_check:
+    mov ah, 0x08      ; Get drive parameters
+    int 0x13
+    jc .not_found     ; If carry flag, drive not present
+
+    mov al, ' '
+    call print_char
+
+    mov al, 'D'
+    call print_char
+    mov al, 'r'
+    call print_char
+    mov al, 'i'
+    call print_char
+    mov al, 'v'
+    call print_char
+    mov al, 'e'
+    call print_char
+    mov al, ' '
+    call print_char
+
+    mov al, dl
+    cmp dl, 0x80
+    je .hdd
+    jmp .fdd
+
+.hdd:
+    mov al, 'H'
+    call print_char
+    mov al, 'D'
+    call print_char
+    mov al, 'D'
+    call print_char
+    jmp .done
+
+.fdd:
+    mov al, 'F'
+    call print_char
+    mov al, 'D'
+    call print_char
+    mov al, 'D'
+    call print_char
+
+.done:
+    call print_newline
+    ret
+
+.not_found:
+    ret
+
 clear_screen:
-    mov ax, 0x0003  ; Set text mode 3 (80x25, 16 colors)
+    mov ax, 0x0003
     int 0x10
     ret
 
@@ -16,13 +74,25 @@ print_string:
     lodsb
     or al, al
     jz .done
-    mov ah, 0x0e
-    int 0x10
+    call print_char
     jmp print_string
 .done:
     ret
 
-hello_msg: db "Hello, World!", 0
+print_char:
+    mov ah, 0x0e
+    int 0x10
+    ret
+
+print_newline:
+    mov al, 0x0d
+    call print_char
+    mov al, 0x0a
+    call print_char
+    ret
+
+hello_msg: db "Welcome to DAS!", 0
+print_warning: db "Disk Application System provides absolutely no warrenty implied or stated", 0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55   
