cb65f80c27d5b0d1ed87e31e37dd822aa2fbf807
therealturtlegod855 <therealturtlegod855@gmail.com>
2025-11-28 17:34:27 -0700

    Refactor bootloader to use print_string function

--- main.asm
+++ main.asm
@@ -1,28 +1,23 @@
+; main.asm - Working 16-bit Hello World Bootloader
+bits 16
 org 0x7c00
 
 start:
-    mov ax, 0x07c0
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    mov sp, 0x7c00
-
-    mov si, hello
-    call print
-
+    mov si, hello_msg
+    call print_string
     jmp $
 
-print:
+print_string:
     lodsb
     or al, al
     jz .done
     mov ah, 0x0e
     int 0x10
-    jmp print
+    jmp print_string
 .done:
     ret
 
-hello: db "Hello, World!", 0
+hello_msg: db "Hello, World!", 0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55   
